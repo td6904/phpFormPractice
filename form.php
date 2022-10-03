2bc5e49ed6e141444e867a5392e727239674cca8
td6904 <107479269+td6904@users.noreply.github.com>
2022-10-03 12:40:24 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form File</title>
+</head>
+<body>
+
+<?php
+
+  echo  $_GET['user_name'];
+
+?>
+
+<form  action="../forms/form.php"  method="post">
+
+<div>
+  <label  for="nom">Nom :</label>
+  <input  type="text"  id="nom"  name="user_name">
+</div>
+
+<div>
+  <label  for="courriel">Courriel :</label>
+    <input  type="email"  id="courriel"  name="user_email">
+</div>
+
+<div>
+  <label  for="message">Message :</label>
+  <textarea  id="message"  name="user_message"></textarea>
+</div>
+
+<div  class="button">
+  <button  type="submit">Envoyer votre message</button>
+</div>
+
+</form>
+</body>
+</html>
+
+
+
